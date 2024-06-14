0bb3f59897d789253c08beff0ebeafc7bd3c6869
AditiSingh1208 <123548974+AditiSingh1208@users.noreply.github.com>
2024-06-14 10:48:56 +0000

    Comment Out

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-function DbConnection() {
-  const DB_URL = process.env.MONGO_URI;
+// function DbConnection() {
+//   const DB_URL = process.env.MONGO_URI;
 
-  mongoose.connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-}
+//   mongoose.connect(DB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+// }
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection Errors"));
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "Connection Errors"));
 
-db.once("open", function(){
-    console.log("DB Connected !!")
-});
+// db.once("open", function(){
+//     console.log("DB Connected !!")
+// });
 
-module.exports = DbConnection;
+// module.exports = DbConnection;
--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const dotenv = require("dotenv");
+// const express = require("express");
+// const dotenv = require("dotenv");
 
-const DbConnection = require("./databaseConnection.js");
+// const DbConnection = require("./databaseConnection.js");
 
-const userRouter = require("./routes/users.js");
-const booksRouter = require("./routes/books.js");
+// const userRouter = require("./routes/users.js");
+// const booksRouter = require("./routes/books.js");
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-DbConnection;
+// DbConnection;
 
-const PORT = 8081;
+// const PORT = 8081;
 
-app.use(express.json());
+// app.use(express.json());
 
-// http://localhost:8081/users/
-http: app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Server is up and running :-)",
-    data: "hey",
-  });
-});
+// // http://localhost:8081/users/
+// http: app.get("/", (req, res) => {
+//   res.status(200).json({
+//     message: "Server is up and running :-)",
+//     data: "hey",
+//   });
+// });
 
-app.use("/users", userRouter);
-app.use("/books", booksRouter);
+// app.use("/users", userRouter);
+// app.use("/books", booksRouter);
 
-app.get("*", (req, res) => {
-  res.status(404).json({
-    message: "This route doesn't exits",
-  });
-});
+// app.get("*", (req, res) => {
+//   res.status(404).json({
+//     message: "This route doesn't exits",
+//   });
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running at port ${PORT}`);
+// });
